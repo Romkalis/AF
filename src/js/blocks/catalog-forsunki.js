3ac0ad5203926608dbb4e22ccdfa09ced5a0599c
Roman Bondarchuk <lisss17@mail.ru>
2024-10-03 20:03:10 +0500

    add slider 2 forsunki block

---
+++ src/js/blocks/catalog-forsunki.js
@@ -0,0 +1,59 @@
+import Splide from "@splidejs/splide";
+
+let forsunkiSplide = null; // Переменная для хранения экземпляра слайдера
+
+const startForsunkiSlider = () => {
+  let windowSize = window.innerWidth; 
+  const forsunkiSplideBlock = document.querySelector('.catalog-forsunki');
+  console.log('forsunkiSplideBlock _______', forsunkiSplideBlock)
+
+  const forsunkiList = forsunkiSplideBlock.querySelector('.catalog-forsunki__list');
+  
+  if (forsunkiList && windowSize < 769) { // Если есть элемент списка и размер окна меньше 769
+    forsunkiSplideBlock.classList.add('splide')
+      if (!forsunkiSplide) { // Инициализируем слайдер, если он еще не был создан
+
+          const splideOptions = {
+              type: "loop",
+              arrows: true,
+              pagination: false,
+              drag: true,
+              paginationKeyboard: true,
+              paginationDirection: true,
+              mediaQuery: "min",
+              perPage: 1,
+              breakpoints: {
+                768: {
+                  destroy: true,
+                },
+              },
+          };
+
+          forsunkiList.classList.add('splide__list'); 
+          forsunkiSplide = new Splide("#splide-forsunki", splideOptions);
+
+          const bar = forsunkiSplideBlock.querySelector( '.my-slider-progress-bar');
+          console.log(bar)
+
+          // настройки прогрессбара
+          forsunkiSplide.on( 'mounted move', function () {
+            let end  = forsunkiSplide.Components.Controller.getEnd() + 1;
+            let rate = Math.min( ( forsunkiSplide.index + 1 ) / end, 1 );
+
+            bar.style.width = String( 100 * rate ) + '%';
+          } );
+
+          forsunkiSplide.mount();
+      }
+  } else {
+    if (forsunkiSplide) { // Если слайдер был инициализирован, уничтожаем
+      forsunkiSplide.destroy();
+      forsunkiSplideBlock?.classList.remove('splide')
+      forsunkiList?.classList.remove('splide__list'); // Удаляем класс
+      forsunkiSplide = null; // Обнуляем флаг слайдер
+    }
+  }
+};
+
+document.addEventListener('DOMContentLoaded', startForsunkiSlider);
+window.addEventListener('resize', startForsunkiSlider); 
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@
 // import '../../node_modules'
 
 //blocks
-import './blocks/catalog-culture'
+import './blocks/catalog-culture.js';
+import './blocks/catalog-forsunki.js'
 
 
 // import './blocks/advantages__card.js'
