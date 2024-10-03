54b0c4b572c50a0faa4a74ba645b938c31f64940
Roman Bondarchuk <lisss17@mail.ru>
2024-10-03 18:44:48 +0500

    add slider 2 cultures

---
+++ src/js/blocks/catalog-culture.js
@@ -0,0 +1,56 @@
+import Splide from "@splidejs/splide";
+
+let cultureSplide = null; // Переменная для хранения экземпляра слайдера
+
+const startCultureSlider = () => {
+  let windowSize = window.innerWidth; 
+  const cultureSplideBlock = document.querySelector('.catalog-culture')
+  let cultureList = cultureSplideBlock.querySelector('.catalog-culture__list');
+  
+  if (cultureList && windowSize < 769) { // Если есть элемент списка и размер окна меньше 769
+    cultureSplideBlock.classList.add('splide')
+      if (!cultureSplide) { // Инициализируем слайдер, если он еще не был создан
+          const splideOptions = {
+              type: "loop",
+              arrows: true,
+              pagination: true,
+              drag: true,
+              paginationKeyboard: true,
+              paginationDirection: true,
+              mediaQuery: "min",
+              perPage: 2,
+              breakpoints: {
+                768: {
+                  destroy: true,
+                },
+              },
+          };
+
+          cultureList.classList.add('splide__list'); 
+          cultureSplide = new Splide("#splide-culture", splideOptions);
+
+          const bar = cultureSplideBlock.querySelector( '.my-slider-progress-bar');
+          console.log(bar)
+
+          // Updates the bar width whenever the carousel moves:
+          cultureSplide.on( 'mounted move', function () {
+            let end  = cultureSplide.Components.Controller.getEnd() + 1;
+            let rate = Math.min( ( cultureSplide.index + 1 ) / end, 1 );
+
+            bar.style.width = String( 100 * rate ) + '%';
+          } );
+
+          cultureSplide.mount();
+      }
+  } else {
+    if (cultureSplide) { // Если слайдер был инициализирован, уничтожаем его
+      cultureSplide.destroy();
+      cultureSplideBlock?.classList.remove('splide')
+      cultureList?.classList.remove('splide__list'); // Удаляем класс, если был добавлен
+      cultureSplide = null; // Обнуляем переменную слайдера
+    }
+  }
+};
+
+document.addEventListener('DOMContentLoaded', startCultureSlider);
+window.addEventListener('resize', startCultureSlider); 
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 // Libraries
-
+// import '../../node_modules'
 
 //blocks
+import './blocks/catalog-culture'
+
 
 // import './blocks/advantages__card.js'
+
+// import Splide from '@splidejs/splide'
+
+// new Splide( '#splide-culture' ).mount();
 
 
