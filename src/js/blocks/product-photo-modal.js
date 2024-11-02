e5c97968a3a06984cb93f0d2d1ded9038b5ac02f
Roman Bondarchuk <lisss17@mail.ru>
2024-11-02 14:27:58 +0500

    addvideo? fix popup with video

--- src/js/blocks/product-photo-modal.js
+++ src/js/blocks/product-photo-modal.js
@@ -3,16 +3,33 @@ const sliderList = document.querySelector(".product__picture-list");
 // сработает если найден элемент и ширина экрана больше 520 пикселей.
 
 if (sliderList && window.innerWidth > 520) {
-  sliderList.addEventListener("click", (evt) => {
-        const modal = document.getElementById('pictureModal')
-        const picture = modal.querySelector('.product-picture')
 
-        picture.src = evt.target.src
+  sliderList.addEventListener("click", (evt) => {
+
+        const modal = document.getElementById('pictureModal')
+
+        const additional = evt.target.cloneNode(true)
+
+        if (additional.classList.contains('product__picture-image')) {
+            
+            additional.classList.remove('product__picture-image')
+            additional.classList.add('product-view')
+
+        } else if (additional.classList.contains('product__picture-video')) {
+            
+            additional.classList.remove('product__picture-video')
+            additional.classList.add('product-view')
+        }
+
+        modal.appendChild(additional)
+ 
 
         modal.showModal()
 
         const closeModal = () => {
+
             modal.close()
+            modal.removeChild(additional)
             modal.removeEventListener('click', closeModal)
         }
         modal.addEventListener('click', closeModal)
