ccf0340ffad63013be70411045f6954a45445419
Roman Bondarchuk <lisss17@mail.ru>
2024-12-16 17:26:32 +0500

    added cart additional text, add upload button

--- build/js/index.bundle.js
+++ build/js/index.bundle.js
@@ -21,46 +21,64 @@ if (clearButton) {
 /***/ 636:
 /***/ (function() {
 
-const cartList = document.querySelector('.cart__orders-list')
+const cartList = document.querySelector(".cart__orders-list");
 
 if (cartList) {
+  function isDiscount(elem) {
+    const item = elem.closest(".cart__orders-item");
+    const currentInput = item.querySelector(".cart__card-counter-field");
+    const discountPrice = item.querySelector(".cart__discount-price");
 
-    cartList.addEventListener('click', (evt) => {
-        
-        // обработка счетчиков по нажатию на кнопку
+    if (currentInput.value >= 100) {
+      discountPrice.classList.remove("visually-hidden");
+    } else {
+      discountPrice.classList.add("visually-hidden");
+    }
+  }
+  const cartItems = document.querySelectorAll(".cart__orders-item");
 
-        if (evt.target.classList.contains('cart__card-counter-button')) {
+  cartItems.forEach((item) => isDiscount(item));
 
-            const goods = evt.target.closest('.cart__card-counter')
-            
-            const currentInput = goods.querySelector('.cart__card-counter-field')
-            
-            switch (evt.target.textContent) {
-                case "+": {
-                    currentInput.value = +currentInput.value + 1
-                    break
-                }
-                case "-": {
-                    if(currentInput.value > 0) {
-                    currentInput.value = +currentInput.value - 1
-                    }
-                    break
-                }
+  cartList.addEventListener("click", (evt) => {
+    // обработка счетчиков по нажатию на кнопку
 
-            }
+    if (evt.target.classList.contains("cart__card-counter-button")) {
+      // const
+      const item = evt.target.closest(".cart__orders-item");
+      const currentInput = item.querySelector(".cart__card-counter-field");
+
+      switch (evt.target.textContent) {
+        case "+": {
+          currentInput.value = +currentInput.value + 1;
+          isDiscount(evt.target);
+          break;
         }
-
-        // удаление товара
-
-        if (
-            evt.target.classList.contains('cart__card-delete-icon') || 
-            evt.target.classList.contains('cart__card-delete-button')
-        ) {
-            const elem = evt.target.closest('.cart__card')
-            elem.remove()
+        case "-": {
+          if (currentInput.value > 0) {
+            currentInput.value = +currentInput.value - 1;
+          }
+          isDiscount(evt.target);
+          break;
         }
-    })
+      }
+    }
+
+    // удаление товара
+
+    if (
+      evt.target.classList.contains("cart__card-delete-icon") ||
+      evt.target.classList.contains("cart__card-delete-button")
+    ) {
+      const elem = evt.target.closest(".cart__card");
+      elem.remove();
+    }
+  });
+
+  cartItems.forEach((item) => {
+    item.addEventListener("change", (evt) => isDiscount(evt.target));
+  });
 }
+
 
 /***/ }),
 
