92abf8b1eb982ef8201b7ef5939d8c5e0b6c2e85
Roman Bondarchuk <lisss17@mail.ru>
2024-12-18 11:43:42 +0500

    added another one listener 2 cart / fix negative keyboard input

--- build/js/index.bundle.js
+++ build/js/index.bundle.js
@@ -24,6 +24,7 @@ if (clearButton) {
 const cartList = document.querySelector(".cart__orders-list");
 
 if (cartList) {
+  
   function isDiscount(elem) {
     const item = elem.closest(".cart__orders-item");
     const currentInput = item.querySelector(".cart__card-counter-field");
@@ -35,11 +36,13 @@ if (cartList) {
       discountPrice.classList.add("visually-hidden");
     }
   }
+  // при загрузке данных проверяем товары кол-во которых больше условия, и показываем плашку со скидкой
   const cartItems = document.querySelectorAll(".cart__orders-item");
-
   cartItems.forEach((item) => isDiscount(item));
 
+
   cartList.addEventListener("click", (evt) => {
+
     // обработка счетчиков по нажатию на кнопку
 
     if (evt.target.classList.contains("cart__card-counter-button")) {
@@ -60,7 +63,7 @@ if (cartList) {
           break;
         }
         case "-": {
-          if (currentInput.value > 0) {
+          if (currentInput.value > 1) {
             currentInput.value = +currentInput.value - 1;
           }
           isDiscount(evt.target);
@@ -68,17 +71,28 @@ if (cartList) {
         }
       }
     }
-
     // удаление товара
-
     if (
       evt.target.classList.contains("cart__card-delete-icon") ||
       evt.target.classList.contains("cart__card-delete-button")
     ) {
       const elem = evt.target.closest(".cart__card");
-      elem.remove();
+      elem?.remove();
     }
   });
+  
+  cartList.onchange = function (evt) {
+    const target = evt.target;
+    // Проверяем, что изменение произошло на поле ввода
+    if (target.classList.contains("cart__card-counter-field")) {
+      const input = target;
+      // Корректируем значение инпута (если оно отрицательное или не числовое)
+      if (+input.value < 0) {
+        input.value = Math.abs(input.value); // Присваиваем минимальное значение
+      }
+      isDiscount(input); // Проверяем скидку
+    }
+  };
 
   cartItems.forEach((item) => {
     item.addEventListener("change", (evt) => isDiscount(evt.target));
