eb11f6242b9d2ac3b96b5c7241f300d2bdc83347
Roman Bondarchuk <lisss17@mail.ru>
2024-12-20 00:10:19 +0500

    fix trouble 7 in troublelist

--- src/js/blocks/cart-counter.js
+++ src/js/blocks/cart-counter.js
@@ -1,39 +1,36 @@
 const cartList = document.querySelector(".cart__orders-list");
 
 if (cartList) {
-  
   function isDiscount(elem) {
     const item = elem.closest(".cart__orders-item");
+
     const currentInput = item.querySelector(".cart__card-counter-field");
     const discountPrice = item.querySelector(".cart__discount-price");
-    const DISCOUNT_STARTS_FROM = 100
+    const discountAmount = +discountPrice.dataset.discountAmount;
 
-    if (DISCOUNT_STARTS_FROM !== 0) {
-      currentInput.value >= DISCOUNT_STARTS_FROM 
+    if (discountAmount && discountAmount !== 0) {
+      currentInput.value >= discountAmount
         ? discountPrice.classList.remove("visually-hidden")
-        : discountPrice.classList.add("visually-hidden")
-  }
+        : discountPrice.classList.add("visually-hidden");
+    }
   }
   // при загрузке данных проверяем товары кол-во которых больше условия, и показываем плашку со скидкой
   const cartItems = document.querySelectorAll(".cart__orders-item");
   cartItems.forEach((item) => isDiscount(item));
 
-
   cartList.addEventListener("click", (evt) => {
-
     // обработка счетчиков по нажатию на кнопку
 
     if (evt.target.classList.contains("cart__card-counter-button")) {
+      evt.preventDefault()
       // const
       const item = evt.target.closest(".cart__orders-item");
       const currentInput = item.querySelector(".cart__card-counter-field");
 
       switch (evt.target.textContent) {
         case "+": {
-          
           if (currentInput.value < 0) {
-            
-            currentInput.value = Math.abs(currentInput.value)
+            currentInput.value = Math.abs(currentInput.value);
           }
 
           currentInput.value = +currentInput.value + 1;
@@ -58,8 +55,8 @@ if (cartList) {
       elem?.remove();
     }
   });
-  
-  cartList.onchange = function (evt) {
+
+  cartList.onchange = function(evt) {
     const target = evt.target;
     // Проверяем, что изменение произошло на поле ввода
     if (target.classList.contains("cart__card-counter-field")) {
@@ -68,7 +65,7 @@ if (cartList) {
       if (+input.value < 0) {
         input.value = Math.abs(input.value); // Присваиваем минимальное значение
       } else if (input.value == 0) {
-        input.value = 1; // 
+        input.value = 1; //
       }
       isDiscount(input); // Проверяем скидку
     }
